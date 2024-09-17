80204896cbf43a8be9898260b7a709bf0a18a75e
laiyj <laiyujun@vip.qq.com>
2024-09-17 14:00:28 +0800

    upd packages

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -41,6 +41,7 @@ UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 #UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/luci-app-wolplus" "main"
 UPDATE_PACKAGE "luci-app-jd-dailybonus" "noiver/luci-app-jd-dailybonus" "master"
 #UPDATE_PACKAGE "luci-app-diskman" "shidahuilang/openwrt-package" "Immortalwrt" "pkg"
+UPDATE_PACKAGE "luci-app-linkease" "shidahuilang/openwrt-package" "Lede" "pkg"
 
 if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
