cc96bb060bc49bb75c657a2dcd3d212c40bdd49e
laiyj <laiyujun@vip.qq.com>
2024-09-18 14:47:45 +0800

    upd app

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -46,7 +46,6 @@ UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
 #UPDATE_PACKAGE "linkmount" "shidahuilang/openwrt-package" "Lede" "pkg"
 #UPDATE_PACKAGE "ffmpeg-remux" "shidahuilang/openwrt-package" "Lede" "pkg"
 #UPDATE_PACKAGE "qbittorrent" "NueXini/NueXini_Packages" "main" "pkg"
-UPDATE_PACKAGE "transmission" "kiddin9/openwrt-packages" "master" "pkg"
 
 if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
