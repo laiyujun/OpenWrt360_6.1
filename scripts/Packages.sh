a6511f5b75620fcecf157a182634fe5895c016b2
laiyj <laiyujun@vip.qq.com>
2024-09-27 10:03:11 +0800

    ad luci-app-aria2

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -52,7 +52,12 @@ UPDATE_PACKAGE "quickstart" "linkease/nas-packages" "master" "pkg"
 UPDATE_PACKAGE "luci-app-quickstart" "linkease/nas-packages-luci" "main" "pkg"
 #UPDATE_PACKAGE "istoreenhance" "linkease/nas-packages" "master" "pkg"
 #UPDATE_PACKAGE "luci-app-istoreenhance" "linkease/nas-packages-luci" "main" "pkg"
-#UPDATE_PACKAGE "istoreenhance" "kiddin9/openwrt-packages" "master" "pkg"
+
+#aria2 && dependency
+UPDATE_PACKAGE "aria2" "kiddin9/openwrt-packages" "master" "pkg"
+UPDATE_PACKAGE "ariang" "kiddin9/openwrt-packages" "master" "pkg"
+UPDATE_PACKAGE "luci-app-aria2" "kiddin9/openwrt-packages" "master" "pkg"
+
 #UPDATE_PACKAGE "qbittorrent" "NueXini/NueXini_Packages" "main" "pkg"
 
 if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
